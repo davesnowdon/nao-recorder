01bc5373ea2ac0ea27198252eece4ed63305393d
Dave Snowdon <dave@davesnowdon.com>
2013-07-07 14:46:41 +0100

    added main application window based on Kivy codeinput.py

---
+++ src/main/python/recorder/main.py
@@ -0,0 +1,122 @@
+'''
+Created on 7 Jul 2013
+
+@author: davesnowdon
+'''
+
+import kivy
+kivy.require('1.7.1') 
+
+from kivy.app import App
+from kivy.extras.highlight import KivyLexer
+from kivy.uix.spinner import Spinner, SpinnerOption
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.codeinput import CodeInput
+from kivy.uix.popup import Popup
+from kivy.properties import ListProperty
+from kivy.core.window import Window
+from pygments import lexers
+from pygame import font as fonts
+import codecs, os
+
+class Fnt_SpinnerOption(SpinnerOption):
+    pass
+
+class LoadDialog(Popup):
+
+    def load(self, path, selection):
+        self.choosen_file = [None, ]
+        self.choosen_file = selection
+        Window.title = selection[0][selection[0].rfind(os.sep)+1:]
+        self.dismiss()
+
+    def cancel(self):
+        self.dismiss()
+
+
+class SaveDialog(Popup):
+
+    def save(self, path, selection):
+        _file = codecs.open(selection, 'w', encoding='utf8')
+        _file.write(self.text)
+        Window.title = selection[selection.rfind(os.sep)+1:]
+        _file.close()
+        self.dismiss()
+
+    def cancel(self):
+        self.dismiss()
+
+class NaoRecorderApp(App):
+
+    files = ListProperty([None, ])
+
+    def build(self):
+        b = BoxLayout(orientation='vertical')
+
+
+        menu = BoxLayout(
+            size_hint_y=None,
+            height='30pt')
+        fnt_name = Spinner(
+            text='DroidSansMono',
+            option_cls=Fnt_SpinnerOption,
+            values=sorted(map(str, fonts.get_fonts())))
+        fnt_name.bind(text=self._update_font)
+        mnu_file = Spinner(
+            text='File',
+            values=('Open', 'SaveAs', 'Save', 'Close'))
+        mnu_file.bind(text=self._file_menu_selected)
+
+        menu.add_widget(mnu_file)
+        b.add_widget(menu)
+
+        self.codeinput = CodeInput(
+            lexer=lexers.PythonLexer(),
+            font_name='data/fonts/DroidSansMono.ttf', font_size=12,
+            text="")
+
+        b.add_widget(self.codeinput)
+
+        return b
+
+    def _update_size(self, instance, size):
+        self.codeinput.font_size = float(size)
+
+    def _update_font(self, instance, fnt_name):
+        instance.font_name = self.codeinput.font_name =\
+            fonts.match_font(fnt_name)
+
+    def _file_menu_selected(self, instance, value):
+        if value == 'File':
+            return
+        instance.text = 'File'
+        if value == 'Open':
+            if not hasattr(self, 'load_dialog'):
+                self.load_dialog = LoadDialog()
+            self.load_dialog.open()
+            self.load_dialog.bind(choosen_file=self.setter('files'))
+        elif value == 'SaveAs':
+            if not hasattr(self, 'saveas_dialog'):
+                self.saveas_dialog = SaveDialog()
+            self.saveas_dialog.text = self.codeinput.text
+            self.saveas_dialog.open()
+        elif value == 'Save':
+            if self.files[0]:
+                _file = codecs.open(self.files[0], 'w', encoding='utf8')
+                _file.write(self.codeinput.text)
+                _file.close()
+        elif value == 'Close':
+            if self.files[0]:
+                self.codeinput.text = ''
+                Window.title = 'untitled'
+
+    def on_files(self, instance, values):
+        if not values[0]:
+            return
+        _file = codecs.open(values[0], 'r', encoding='utf8')
+        self.codeinput.text = _file.read()
+        _file.close()
+
+
+if __name__ == '__main__':
+    NaoRecorderApp().run()
