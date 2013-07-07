ac1f6c71c08e6fe00e51f595f89b6cb4f75df14f
Dave Snowdon <dave@davesnowdon.com>
2013-07-07 15:52:57 +0100

    Fixed refresh-ext-projects.sh so that the specifications of what files to copy can have sub-dirs. For example src|bar/baz means copy the contents of foo/bar/baz into dest/bar/baz

--- refresh-ext-projects.sh
+++ refresh-ext-projects.sh
@@ -5,15 +5,18 @@
 
 PYTHON_SRC="src/main/python"
 
-#EXT_PROJECTS="../naoutil/naoutil/src/main/python/naoutil ../FluentNao/src/main/python/fluentnao ../FluentNao/src/main/python/fluentnao/core ../FluentNao/src/main/python/fluentnao/memory"
-#EXT_PROJECTS="../naoutil/naoutil/src/main/python/naoutil"
+EXT_PROJECTS="../naoutil/naoutil/src/main/python|naoutil ../FluentNao/src/main/python|fluentnao ../FluentNao/src/main/python|fluentnao/core"
 
 
-for ppath in $EXT_PROJECTS
+for pspec in $EXT_PROJECTS
 do
-    pname=`basename $ppath`
+    # split base|sub-path into directory and sub folder
+    IFS='|' read -ra proj <<< "$pspec"
+    ploc=${proj[0]}
+    pname=${proj[1]}
+    ppath="${ploc}/${pname}"
     dest="${PYTHON_SRC}/${pname}"
-    
+
     # delete old python files
     rm -f ${dest}/*.py*
 
