e1c86cbe20b7afe22ee8eacfb50d6631ac329911
Don Najd <dnajd7@gmail.com>
2013-07-10 23:10:35 -0700

    evaluates up

--- src/main/python/translators/fluentnao/core.py
+++ src/main/python/translators/fluentnao/core.py
@@ -9,18 +9,101 @@ class FluentNaoTranslator(object):
 	def detect_command(self, joint_dict):
 		commands = []
 
+		# arms
+		commands = commands + self.detect_arms(joint_dict)
+
+		# return
+		return commands
+
+	def detect_arms(self, joint_dict):
+
 		# arms forward?
-		commands = commands + self.eval_forward(joint_dict)
+		arms_commands = self.eval_forward(joint_dict)
+
+		# arms up?
+		if len(arms_commands) == 0:
+			arms_commands = self.eval_up(joint_dict)
 
 		# arms out?
 
-		# arms up?
-
 		# arms back?
 
-		# return commands
+		return arms_commands
+
+	def eval_up(self, joint_dict):
+		commands = []
+
+		# check both arms
+		result = self.is_arms_up(joint_dict)
+		if result:
+			commands.append(result)
+		else:
+			# check left
+			left = self.is_left_arm_up(joint_dict)
+			if left:
+				commands.append(left)
+			
+			# check right
+			right = self.is_right_arm_up(joint_dict)	
+			if right:
+				commands.append(right)
 		return commands
 
+	def is_left_arm_up(self, joint_dict):
+
+		# left arm
+		l_should_pitch = math.degrees(joint_dict['LShoulderPitch'])
+		l_should_roll = math.degrees(joint_dict['LShoulderRoll'])
+
+		# left pitch range(90)
+		if l_should_pitch <= -45 and l_should_pitch >= -110:
+			l_pitch_offset = round(-90 - l_should_pitch)
+
+			# LEFT: match roll
+			if l_should_roll <= 45:
+				l_roll_offset = round(-l_should_roll)
+				return ("arms.left_up", [l_pitch_offset, l_roll_offset]) 
+		return None
+
+	def is_right_arm_up(self, joint_dict):
+
+		# right arm
+		r_should_pitch = math.degrees(joint_dict['RShoulderPitch'])
+		r_should_roll = math.degrees(joint_dict['RShoulderRoll'])
+
+		# RIGHT: match pitch
+		if r_should_pitch <= -45 and r_should_pitch >= -110:
+			r_pitch_offset = round(-90 - r_should_pitch)
+
+			# LEFT: match roll
+			if r_should_roll >= -45:
+				r_roll_offset = round(r_should_roll)
+				return ("arms.right_up", [r_pitch_offset, r_roll_offset])
+		return None
+
+	def is_arms_up(self, joint_dict):
+		# check each arm
+		left = self.is_left_arm_up(joint_dict)
+		right = self.is_right_arm_up(joint_dict)
+
+		# both up?
+		if left and right:
+			
+			# compare offsets
+			l_pitch_offset = left[1][0]
+			l_roll_offset = left[1][1]
+			r_pitch_offset = right[1][0]
+			r_roll_offset = right[1][1]
+
+			# allow within 10 degrees
+			pitch_diff = abs(l_pitch_offset - r_pitch_offset)
+			roll_diff = abs(l_roll_offset - r_roll_offset)
+			if pitch_diff <= 10 and roll_diff <= 10:
+
+				# reduce
+				return ("arms.up", [l_pitch_offset, l_roll_offset])
+
+		return None
 
 
 	def eval_forward(self, joint_dict):
@@ -81,6 +164,7 @@ class FluentNaoTranslator(object):
 
 		# both forward?
 		if left and right:
+
 			# compare offsets
 			l_pitch_offset = left[1][0]
 			l_roll_offset = left[1][1]
--- src/test/python/recorder_tests/test_fluentnao_translator.py
+++ src/test/python/recorder_tests/test_fluentnao_translator.py
@@ -7,7 +7,7 @@ import unittest
 import math
 
 from translators.fluentnao.core import FluentNaoTranslator
-from testutil import POSITION_ZERO, make_joint_dict
+from testutil import POSITION_ZERO, POSITION_ARMS_UP, make_joint_dict
 
 
 def get_translator():
@@ -35,6 +35,34 @@ class TestDetectArms(unittest.TestCase):
 
         # pitch offset
         desired_pitch_offset = round(math.degrees(joint_dict['LShoulderPitch']))
+        actual_pitch_offset = first_tuple[1][0]
+        self.assertEqual(desired_pitch_offset, actual_pitch_offset, "Should match pitch offset")
+
+        # roll offset
+        desired_roll_offset = round(-math.degrees(joint_dict['LShoulderRoll']))
+        actual_roll_offset = first_tuple[1][1]
+        self.assertEqual(desired_roll_offset, actual_roll_offset, "Should match roll offset")
+
+    def testArmsUp(self):
+        
+        # joint positions
+        joint_dict = make_joint_dict(POSITION_ARMS_UP)
+
+        # call function
+        result = get_translator().detect_command(joint_dict)
+        self.assertEqual(len(result), 1, "Should get one tuple")
+
+        # expect one tuple
+        first_tuple = result[0]
+
+        # command
+        print 'first: ' + str(first_tuple)
+        print 'cmd: {0}(0, {1},{2})'.format(first_tuple[0], first_tuple[1][0], first_tuple[1][1])
+        command = first_tuple[0]
+        self.assertEqual("arms.up", command, "Should detect command arms up")
+
+        # pitch offset
+        desired_pitch_offset = round(-90 - math.degrees(joint_dict['LShoulderPitch']))
         actual_pitch_offset = first_tuple[1][0]
         self.assertEqual(desired_pitch_offset, actual_pitch_offset, "Should match pitch offset")
 
