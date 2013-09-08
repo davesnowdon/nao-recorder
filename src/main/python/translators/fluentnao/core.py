8645708a855560bfa60e03472d015c3e5e1844e6
Don Najd <dnajd7@gmail.com>
2013-09-08 14:47:57 -0700

    attmempt to do head up, down, center

--- src/main/python/translators/fluentnao/core.py
+++ src/main/python/translators/fluentnao/core.py
@@ -83,6 +83,25 @@ COMMANDS = [
                         ['headYaw']
                         ),
 
+            CommandSpec('up', 'head',
+                        set(['HeadPitch']),
+                        [Transform(linear, 'HeadPitch', 'headPitch', [-1, -38)],
+                        [Constraint(in_range, [-38, -10, 'HeadPitch'])],
+                        ['headPitch']
+                        ),
+            CommandSpec('down', 'head',
+                        set(['HeadPitch']),
+                        [Transform(linear, 'HeadPitch', 'headPitch', [1, -29])],
+                        [Constraint(in_range, [10, 29, 'HeadPitch'])],
+                        ['headPitch']
+                        ),
+            CommandSpec('center', 'head',
+                        set(['HeadPitch']),
+                        [Transform(linear, 'HeadPitch', 'headPitch', [1, 0])],
+                        [Constraint(in_range, [-11, 9, 'HeadPitch'])],
+                        ['headPitch']
+                        ),
+
             # arms both
             CommandSpec('forward', 'arms',
                         set(['LShoulderPitch', 'LShoulderRoll', 'RShoulderPitch', 'RShoulderRoll']),
